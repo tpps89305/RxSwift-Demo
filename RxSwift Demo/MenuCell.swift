64b99386d634298f35d2e3ca04b94f8426f98705
Dispy-Mac <tpps89305@hotmail.com>
2022-09-12 23:57:17 +0800

    Build menu page

---
+++ RxSwift Demo/MenuCell.swift
@@ -0,0 +1,25 @@
+//
+//  MenuCell.swift
+//  RxSwift Demo
+//
+//  Created by 楊朝富 on 2022/9/12.
+//
+
+import UIKit
+
+class MenuCell: UITableViewCell {
+
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ RxSwift Demo/MenuViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MenuViewController.swift
+//  RxSwift Demo
+//
+//  Created by 楊朝富 on 2022/9/12.
+//
+
+import Foundation
+import UIKit
+
+class ManuViewController: UITableViewController {
+    
+    let menus = ["Login", "ImagePicker", "AdvenceTableView", "Calculator"]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menus.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath) as! MenuCell
+        cell.labelTitle.text = menus[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            self.performSegue(withIdentifier: "gotoLogin", sender: nil)
+        default:
+            NSLog("Cannot found page with index = %d", indexPath.row)
+        }
+    }
+}
